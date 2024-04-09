4454aec584b3fd86e8930af0981ed267f9ab4a2b
swastik <swastikasija072@gmail.com>
2024-04-09 22:25:22 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Numbers from "./components/Numbers";
+import Team from "./components/Team";
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <Navbar />
       <Hero/>
       <Numbers/>
-      
+      <Team/>
     </>
   );
 }
